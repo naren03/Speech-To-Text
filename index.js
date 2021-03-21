e8c568f28319f3b3a1a13090a61176b916762e2a
Narendra <naren3001singh@gmail.com>
2021-03-22 02:24:11 +0530

    dsfef

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 console.log('Number:' + randomNumber);
 //all event listeners
 const speakBtn = document.getElementById('speak');
-const output = document.querySelector('.result');
 const mainBox = document.querySelector('.container');
 
 //clicking on speak now
-speakBtn.addEventListener('click', (e) => {
-	e.preventDefault();
-	myRecognition.start();
-});
+// speakBtn.addEventListener('click', (e) => {
+// 	e.preventDefault();
+myRecognition.start();
+// });
 
 myRecognition.addEventListener('result', (event) => {
 	let userInput = event.results[0][0].transcript;
@@ -26,51 +25,36 @@ myRecognition.addEventListener('end', () => {
 	myRecognition.start();
 });
 
+document.body.addEventListener('click', (e) => {
+	if ((e.target.id = 'play-again')) {
+		window.location.reload();
+	}
+});
 //functions
 
 function chkNumber(msg) {
 	const number = +msg;
-	speakBtn.style.display = 'none';
+	// speakBtn.style.display = 'none';
+
+	//display what user said
+	mainBox.appendChild = `<p>You Said:</p><span>${number}</span>`;
 
 	if (Number.isNaN(number)) {
-		let answer = document.createElement('div');
-
-		answer.setAttribute('class', 'user-input');
-
-		answer.innerHTML = `<p>You Said:</p><span>${number}</span><p class="message">It Is Not Valid Number</p>`;
+		// let answer = document.createElement('div');
+		// answer.setAttribute('class', 'user-input');
+		// answer.innerHTML = `<p>You Said:</p><span>${number}</span><p class="message">It Is Not Valid Number</p>`;
+		mainBox.appendChild = `<p class="message">It Is Not Valid Number</p>`;
 	} else if (number === randomNumber) {
-		mainBox.innerHTML = `<div class="answer"><h1>Congrats! You Have Guessed The Number</h1><h3>It Was ${number}</h3></div>`;
+		document.body.innerHTML = `<div class="container"><div class="answer"><h1>Congrats! You Have Guessed The Number</h1><h3>It Was ${number}</h3><a id="play-again>Play Again</a></div></div>`;
 
-		let playAgain = document.createElement('a');
-
-		playAgain.innerHTML = 'Play Again';
-
-		playAgain.addEventListener('click', () => {
-			window.location.reload();
-		});
-
-		mainBox.children[0].appendChild(playAgain);
-	} else if (randomNumber > number) {
-		let answer = document.createElement('div');
-
-		answer.setAttribute('class', 'user-input');
-
-		answer.innerHTML = `<p>You Said:</p><span>${number}</span><p class="message">Go Higher</p>`;
-		output.appendChild(answer);
-	} else if (randomNumber < number) {
-		let answer = document.createElement('div');
-
-		answer.setAttribute('class', 'user-input');
-
-		answer.innerHTML = `<p>You Said:</p><span>${number}</span><p class="message">Go Lower</p>`;
-
-		output.appendChild(answer);
+		// mainBox.children[0].appendChild(playAgain);
+	} else if (number < randomNumber) {
+		mainBox.appendChild = `<p class="message">Go Higher</p>`;
+	} else if (number > randomNumber) {
+		mainBox.appendChild = `<p class="message">Go Lower</p>`;
 	} else if (number > 100 || number < 1) {
-		let answer = document.createElement('div');
-
-		answer.setAttribute('class', 'user-input');
-
-		answer.innerHTML = `<p>You Said:</p><span>${number}</span><p class="message">Number Should Be Between 1-100</p>`;
+		mainBox.appendChild = `<p class="message">Number Should Be Between 1-100</p>`;
 	} else {
+		return;
 	}
 }
